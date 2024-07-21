9a89bfb03622021736bc8525c651baa6203f2488
Tamerlan <147836444+Tamer30@users.noreply.github.com>
2024-07-21 20:06:45 +0300

    Update keras.py

--- keras.py
+++ keras.py
@@ -33,3 +33,33 @@ model = keras.models.Sequential()
 model.add(keras.layers.Dense(units=1, input_dim=features_train.shape[1]))
 model.compile(loss='mean_squared_error', optimizer='sgd')
 model.fit(features_train, target_train, verbose=2, epochs=5 ,validation_data=(features_valid, target_valid))
+
+
+#######################################
+
+import pandas as pd
+from tensorflow import keras
+from sklearn.metrics import accuracy_score
+
+
+df = pd.read_csv('/datasets/train_data_n.csv')
+df['target'] = (df['target'] > df['target'].median()).astype(int)
+features_train = df.drop('target', axis=1)
+target_train = df['target']
+
+df_val = pd.read_csv('/datasets/test_data_n.csv')
+df_val['target'] = (df_val['target'] > df['target'].median()).astype(int)
+features_valid = df_val.drop('target', axis=1)
+target_valid = df_val['target']
+
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(units=1, input_dim=features_train.shape[1], 
+                             activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='sgd')
+model.fit(features_train, target_train, epochs=5, verbose=0,
+          validation_data=(features_valid, target_valid))
+
+
+predictions = model.predict(features_valid) > 0.5
+score = accuracy_score(target_valid, predictions)
+print("Accuracy:", score) 
